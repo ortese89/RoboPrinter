e927d40a679fa374055e5d43083a8770f34bd475
Coditech <i.annoni@coditech.it>
2024-10-21 17:15:47 +0200

    IA. sblocco funzione per avvio ciclo da stringa etichetta

--- BackEnds/BackEnds.RoboPrinter/Services/ExternalCommunicationBase.cs
+++ BackEnds/BackEnds.RoboPrinter/Services/ExternalCommunicationBase.cs
@@ -93,7 +93,7 @@ public abstract class ExternalCommunicationBase : IExternalDeviceCommunication
     {
         var matchedCommand = CSECommands.Keys.FirstOrDefault(k => message.StartsWith(k));
 
-        if (matchedCommand is null) return;
+        //if (matchedCommand is null) return;
 
         switch (matchedCommand)
         {
